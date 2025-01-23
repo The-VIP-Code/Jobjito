03ebcfbd5d062f5d212cdf057a0c351c933f27d3
Acacia-Sanchez-Xanina <43806224+Acacia-Sanchez@users.noreply.github.com>
2025-01-23 11:46:48 +0100

    first commit

---
+++ src/main/java/dev/thevipcode/jobjito/JobjitoApplication.java
@@ -0,0 +1,13 @@
+package dev.thevipcode.jobjito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobjitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobjitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/thevipcode/jobjito/JobjitoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thevipcode.jobjito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobjitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
